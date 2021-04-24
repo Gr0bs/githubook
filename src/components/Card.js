e2be2bdbbb77ab7f6452a9ac299ee6a17c1cfb72
Gr0bs <gr0bs@outlook.com>
2021-04-24 12:03:46 +0200

    card update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,34 @@
 import '../styles/card.scss'
 import Profile from './Profile'
+import {useState, useEffect} from 'react'
 
-const Card = ({event, description, date}) => {
+const Card = (props) => {
+
+    const {event, description, date, image, username, subject} = props
 
     let time = date // Destructre for having hours or min or days
 
+    const [eventName, setEventName] = useState('')
+
+    useEffect(() => {
+        switch(event) {
+            case "PublicEvent": 
+                setEventName('past public the repo')
+                break;
+            case "CreateEvent":
+                setEventName('create the repo ')
+                break;
+            default:
+                setEventName(event)
+            }
+    },[event])
+
     return ( 
             <div className="card">
-                <Profile image={image}/>
+                <Profile image={image} username={username}/>
                 <div className="card__event">
-                    <p>{event}</p>
-                    {description && (<cite>{description}</cite>)}
+                    <p>{eventName} <strong>{subject}</strong></p>
+                    {description && (<cite>"{description}"</cite>)}
                     {time && (<span>{time}</span>)}
                 </div>
            </div>
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,3 @@
-import {useEffect, useState} from 'react'
 import '../styles/feed.scss'
 import Card from './Card'
 import useFetch from './useFetch'
@@ -14,7 +13,13 @@ const Feed = () => {
             {error && <span> {error}</span>}
             {data && data.map(event => (
                 <div className="feed__card">
-                    <Card event={event.type} description={event.payload.description}/>
+                    <Card 
+                        event={event.type} 
+                        description={event.payload.description}
+                        username={event.actor.login}
+                        image={event.actor.avatar_url}
+                        subject={event.repo.name}
+                    />
                 </div>
             ))
             }
