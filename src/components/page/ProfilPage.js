e7d0dfaa7e15a2b759595931822a257167a848b1
Gr0b <gr0bs@outlook.com>
2021-05-06 17:33:59 +0200

    profilePage - github infos

--- src/components/page/ProfilPage.js
+++ src/components/page/ProfilPage.js
@@ -8,21 +8,25 @@ const ProfilPage = () => {
 
     const {username} = useParams()
     const {data: userProfil, error} = useFetch(`https://api.github.com/users/${username}`)
+    const {data: starredRepo, error2} = useFetch(`https://api.github.com/users/${username}/starred`)
 
-    //Social informations
+    //Social informations   
 
 
     return ( 
         <div className="profilePage">
             {error && <span>{error}</span>}
             {userProfil && (
+                // HEADER BANNER WITH USER INFO
                 <header>
                     <Profile 
                         username={userProfil.login}
                         name={userProfil.name}
                         image={userProfil.avatar_url}
-                        size='medium'
+                        size='high'
                         />
+
+                    {/* USER SOCIAL LINKS & INFOS */}
                     <ul className="user__social">
                         {userProfil.twitter_username && (
                             <li><a href={`https://twitter.com/${userProfil.twitter_username}`}>X</a></li>
@@ -33,6 +37,14 @@ const ProfilPage = () => {
                         { userProfil.email && (
                             <li><a href={`https://twitter.com/${userProfil.email}`}>X</a></li>
                         )}
+                    </ul>
+
+                    {/* USER GITHUB INFORMATIONS */}
+                    <ul className="user__github">
+                        <li>Followers <span>{userProfil.followers}</span></li>
+                        <li>Following <span>{userProfil.following}</span></li>
+                        <li>Repos <span>{userProfil.public_repos}</span></li>
+                        <li>Starred Repos <span>{starredRepo.length}</span></li>
                     </ul>
                 </header>
             )}
