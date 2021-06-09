6f28fa38cf2cb444558a3412a7678a646d5ca5b2
Rob <gr0bs@outlook.com>
2021-06-09 16:32:54 +0200

    :todo: Firebse Auth fonctional

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
-createApp(App).use(router).mount('#app')
+const firebaseConfig = {
+    apiKey: "AIzaSyDUTlmNlwtif0PS0HLKfdmXPTjnjmaSyI0",
+    authDomain: "githubsocial.firebaseapp.com",
+    projectId: "githubsocial",
+    storageBucket: "githubsocial.appspot.com",
+    messagingSenderId: "513777776854",
+    appId: "1:513777776854:web:d52ac4716e5b61ff407177",
+    measurementId: "G-7E7572WSTD"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+let app
+
+firebase.auth().onAuthStateChanged(user => {
+    if(!app){
+        createApp(App).use(router).mount('#app')
+    }
+})
+
